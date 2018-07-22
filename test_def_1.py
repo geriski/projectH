5a622b61e21bf29ba3fcbc232d611b8d98c39e41
Attila Kiss <atttila.kiss@gmail.com>
2018-07-22 22:23:39 +0200

    commit #32874
    próbálom a 2 kurva ágat közös nevezőre hozni, de egyelőre kurvára nem akar összejönni

---
+++ test_def_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 21:24:25 2018
+
+@author: attilakiss
+"""
+from car_select import car_selection
+
+selected_car = car_selection()
+
+link = selected_car
+print(link)
