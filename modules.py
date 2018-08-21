58a55ee25986f1ffd0324d6797e9e5c839ff35b9
geriski <39804245+geriski@users.noreply.github.com>
2018-08-21 16:00:20 +0200

    expanding get_url_list(link) modul

--- modules.py
+++ modules.py
@@ -7,13 +7,17 @@ from lxml import html
 
 def get_url_list(link):
     """
-    Get the url lists, where the actual url of the cars can be found. If there is 1 page, append just that one.
+    Bulid a dictionary with the car ID's and the webpages URL addresses.
     """
     modelpage = requests.get(link)
     tree = html.fromstring(modelpage.content)
+    
+    # Get the url lists, where the actual url of the cars can be found.
     maxlist = tree.xpath('//li[@class="last"]/a/@href')
     try:
         string = maxlist[0]
+        
+    #If there is 1 page, append just that one.
     except IndexError:
         pagelists = [link]
     else:
@@ -24,4 +28,31 @@ def get_url_list(link):
         pagelists = []
         for maxpage in range(1,int(maxpage)+1):
             pagelists.append(str(link) + "page" + str(maxpage))
-    return pagelists
+
+    cars_pages_ids={}
+    
+    for pagelist in pagelists:
+        pagelis = requests.get(pagelist)
+        tree = html.fromstring(pagelis.content)
+        
+        #Get the url's of the cars
+        carpages = tree.xpath('//a[@class=""]/@href')
+        
+        #Remove dupplicates
+        hirdetes_kodok = set(carpages)
+        
+        cars_pages_id={}
+        
+        #Making the ID-s from the URL links
+        for hirdetes_kod in hirdetes_kodok:
+            leng = hirdetes_kod.rfind('-')
+            kod = hirdetes_kod[(leng)+1:]
+            if kod.isdigit():
+                cars_pages_id[kod] = hirdetes_kod
+            else:
+                cars_pages_id[kod] = 'error'
+    
+        #Build a dictionary with the webpages and car ID's
+    
+        cars_pages_ids.update(cars_pages_id)
+    return cars_pages_ids
